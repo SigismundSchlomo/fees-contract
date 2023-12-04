b8506ba96f79c02d1452cf02a7f18b461b200465
Alex Kuznietsov <o.kuznietsov@inc4.net>
2023-12-04 14:14:37 +0000

    Add deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,17 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  networks: {
+    local: {
+      url: "http://0.0.0.0:8545",
+      accounts: [
+        "0x80f702eb861f36fe8fbbe1a7ccceb04ef7ddef714604010501a5f67c8065d446",
+      ],
+      hardfork: "byzantium",
+    },
+  },
+
+  solidity: "0.8.21",
 };
 
 export default config;
